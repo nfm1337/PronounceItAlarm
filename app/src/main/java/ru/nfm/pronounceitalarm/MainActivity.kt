b8c4780654050c1f8755eb5af3a0179b2afacf42
NFM <daniel.olovianniy@gmail.com>
2023-09-04 19:30:15 +0300

    initial commit

---
+++ app/src/main/java/ru/nfm/pronounceitalarm/MainActivity.kt
@@ -0,0 +1,45 @@
+package ru.nfm.pronounceitalarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.nfm.pronounceitalarm.ui.theme.PronounceItAlarmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PronounceItAlarmTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PronounceItAlarmTheme {
+        Greeting("Android")
+    }
+}
