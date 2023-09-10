9ff3cc0359ba0721e9f0d9be12983e00a6c4a93a
NFM <daniel.olovianniy@gmail.com>
2023-09-10 12:15:11 +0300

    added permissions library, updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,15 +64,16 @@ dependencies {
 
     //noinspection GradleDependency
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.7.1")
+    implementation("androidx.navigation:navigation-compose:2.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.code.gson:gson:2.10")
+    implementation("com.google.accompanist:accompanist-permissions:0.33.1-alpha")
 
     // Firebase
     implementation("com.google.firebase:firebase-crashlytics:18.4.1")
